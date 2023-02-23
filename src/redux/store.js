c6695a97a6f447e2f413a53c0ae69f879ad77053
Belkina23 <23zakat@gmail.com>
2023-02-23 17:41:32 +0200

    Update store.js

--- src/redux/store.js
+++ src/redux/store.js
@@ -11,7 +11,7 @@ import {
 } from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
 import contactsReducer from './contactsSlice';
-import filtersReduser from './filtersSlice';
+import filterReduser from './filtersSlice';
 import { combineReducers } from 'redux';
 
 
@@ -22,7 +22,7 @@ const persistConfig = {
 
 const rootReducer = combineReducers({
   contacts: contactsReducer,
-  filter: filtersReduser,
+  filter: filterReduser,
 });
 
 const persistedReducer = persistReducer(persistConfig, rootReducer);
